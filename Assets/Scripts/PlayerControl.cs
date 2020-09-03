924131c8bd48cb295b2a3dcb82b55a1d54dea8bf
Calum Bell <calumabell@googlemail.com>
2020-09-03 19:47:41 +0100

    created PlayerControl script

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float verticalInput;
+    private float horizontalInput;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+    }
+}
