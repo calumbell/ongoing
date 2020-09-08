302f1261e2d30de5e625e6c81aea21a6533e2e33
Calum Bell <calumabell@googlemail.com>
2020-09-08 20:45:31 +0100

    added PlayerCollider script for detecting stair collisions

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("StairDown")) {
+            Debug.Log("These stairs are going down!");
+        }
+    }
+}
