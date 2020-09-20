1cc1e678fe1f002913f58f1fad14c4204741cf74
Calum Bell <calumabell@googlemail.com>
2020-09-20 18:52:49 +0100

    added menu/dungeon transition

---
+++ Assets/Scripts/Dungeon Scripts/DungeonTransitionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonTransitionManager : MonoBehaviour
+{
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeTime;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panal = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panal, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeTime;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            StartCoroutine(FadeCoroutine());
+    }
+
+    public IEnumerator FadeCoroutine()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject fadeIn = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+
+        yield return new WaitForSeconds(fadeTime);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Dungeon");
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
