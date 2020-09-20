d0351e6a2c87bdd85a5aa90d9733bda61241d7bf
Calum Bell <calumabell@googlemail.com>
2020-09-20 20:03:54 +0100

    added transitions between floors

--- Assets/Scripts/Dungeon Scripts/DungeonTransitionManager.cs
+++ Assets/Scripts/Dungeon Scripts/DungeonTransitionManager.cs
@@ -13,7 +13,22 @@ public class DungeonTransitionManager : MonoBehaviour
         if (fadeInPanel != null)
         {
             GameObject panal = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
-            Destroy(panal, 1);
+            Destroy(panal, fadeTime);
         }
+    }
+
+    public void OnStairsInteractEventReceived(int input)
+    {
+        StartCoroutine(fadeCoroutine());
+    }
+
+    public IEnumerator fadeCoroutine()
+    {
+        GameObject fadeIn = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+        yield return new WaitForSeconds(fadeTime);
+        GameObject fadeOut = Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity) as GameObject;
+        Destroy(fadeIn);
+        yield return new WaitForSeconds(fadeTime);
+        Destroy(fadeOut);
     }
 }
